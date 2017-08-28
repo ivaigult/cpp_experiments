cf1744b7bbfff840754e0f4da493289df2f146b3
Ivan Vaigult <i.vaigult@gmail.com>
2017-08-28 09:05:56 +0100

    First preproc drop

---
+++ pp_gen.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define UNARY_FUNCTION(func_name, math_size, operation) do {            \
+        for(int x = 0; x < math_size; ++x) {                            \
+            printf("#define __" #func_name "_%d %d\n", x, (operation)); \
+        }                                                               \
+        printf("#define " #func_name "(x) CAT2(__" #func_name "_, x)\n");   \
+    } while(0)
+
+#define BINARY_FUNCTION(func_name, math_size, operation) do {           \
+        for(int x = 0; x < math_size; ++x) {                            \
+            for(int y = 0; y < math_size; ++y) {                        \
+                printf("#define __" #func_name "_%d_%d %d\n", x, y, (operation)); \
+            }                                                           \
+        }                                                               \
+        printf("#define " #func_name "(x, y) CAT4(__" #func_name "_, x , _, y)\n"); \
+    } while(0)
+
+int main(int argc, char** argv)
+{
+    int math_size = 4;
+    if (argc > 1) {
+        math_size = atoi(argv[1]);
+    }
+
+    for(int ii = 0; ii < math_size; ++ii) {
+        printf("#define PP_UINT_%d %d\n", ii, ii);
+    }
+
+
+    printf("#define CAT2(x0, x1)         x0 ## x1\n");
+    printf("#define CAT3(x0, x1, x2)     x0 ## x1 ## x2\n");
+    printf("#define CAT4(x0, x1, x2, x3) x0 ## x1 ## x2 ## x3\n");
+    
+    UNARY_FUNCTION(PP_BOOL, math_size, x != 0);
+    printf("#define PP_NOT(x) __PP_NOT(PP_BOOL(x))\n");
+    printf("#define __PP_NOT(x) CAT2(__PP_NOT_, x)\n");
+    printf("#define __PP_NOT_1 0\n");
+    printf("#define __PP_NOT_0 1\n");
+
+    BINARY_FUNCTION(PP_EQ,  math_size, x == y);
+    printf("#define PP_NEQ(x, y) PP_NOT(PP_EQ(x, y))\n");
+    
+    BINARY_FUNCTION(PP_ADD, math_size, (x + y) % math_size);
+    BINARY_FUNCTION(PP_SUB, math_size, (x + math_size - y) % math_size);
+
+    printf("#define PP_INC(x) PP_ADD(x, 1)\n");
+    printf("#define PP_DEC(x) PP_SUB(x, 1)\n");
+    
+    BINARY_FUNCTION(PP_MUL, math_size, (x * y) % math_size);
+    BINARY_FUNCTION(PP_DIV, math_size, (x / (y ? y : 1) ) % math_size);
+
+    printf("#define PP_LEN(...) __PP_LEN(~, ## __VA_ARGS__");
+    for(int i = 0; i < math_size; ++i) {
+        printf(", %d", math_size - i - 1); 
+    }
+    printf(", 0)\n");
+    printf("#define __PP_LEN(u, ");
+    for(int i = 0; i < math_size - 1; ++i) {
+        printf("%sx%d", i ? ", " : "", i);
+    }
+    printf(", len, ...) len\n");
+
+    printf("#define PP_IF(cond, then_do, else_do) __PP_IF(PP_BOOL(cond), then_do, else_do)\n");
+    printf("#define __PP_IF(cond, then_do, else_do) CAT2(__PP_IF_, cond)(then_do, else_do)\n");
+    printf("#define __PP_IF_1(then_do, else_do) then_do\n");
+    printf("#define __PP_IF_0(then_do, else_do) else_do\n");
+
+    printf("#define PP_EVAL(...) __PP_EVAL_1024(__VA_ARGS__)\n");
+    printf("#define __PP_EVAL_1(...) __VA_ARGS__\n");
+    printf("#define __PP_EVAL_2(...)    __PP_EVAL_1(__PP_EVAL_1(__PP_EVAL_1(__VA_ARGS__)))\n");
+    printf("#define __PP_EVAL_4(...)    __PP_EVAL_2(__PP_EVAL_2(__PP_EVAL_2(__VA_ARGS__)))\n");
+    printf("#define __PP_EVAL_8(...)    __PP_EVAL_4(__PP_EVAL_4(__PP_EVAL_4(__VA_ARGS__)))\n");
+    printf("#define __PP_EVAL_16(...)   __PP_EVAL_8(__PP_EVAL_8(__PP_EVAL_8(__VA_ARGS__)))\n");
+    printf("#define __PP_EVAL_32(...)   __PP_EVAL_16(__PP_EVAL_16(__PP_EVAL_16(__VA_ARGS__)))\n");
+    printf("#define __PP_EVAL_64(...)   __PP_EVAL_32(__PP_EVAL_32(__PP_EVAL_32(__VA_ARGS__)))\n");
+    printf("#define __PP_EVAL_128(...)  __PP_EVAL_64(__PP_EVAL_64(__PP_EVAL_64(__VA_ARGS__)))\n");
+    printf("#define __PP_EVAL_256(...)  __PP_EVAL_128(__PP_EVAL_128(__PP_EVAL_128(__VA_ARGS__)))\n");
+    printf("#define __PP_EVAL_512(...)  __PP_EVAL_256(__PP_EVAL_256(__PP_EVAL_256(__VA_ARGS__)))\n");
+    printf("#define __PP_EVAL_1024(...) __PP_EVAL_512(__PP_EVAL_512(__PP_EVAL_512(__VA_ARGS__)))\n");
+    
+
+    printf("#define PP_EMPTY()\n");
+    printf("#define PP_DEFER_1(fn) fn PP_EMPTY()\n");
+    printf("#define PP_DEFER_2(fn) fn PP_EMPTY PP_EMPTY()()\n");
+    printf("#define PP_DEFER_3(fn) fn PP_EMPTY PP_EMPTY PP_EMPTY()()()\n");
+    printf("#define PP_DEFER_4(fn) fn PP_EMPTY PP_EMPTY PP_EMPTY PP_EMPTY()()()()\n");
+
+    printf("#define PP_FOR(from, to, fn) PP_EVAL(__PP_FOR(from, to, fn))\n");
+    printf("#define __PP_FOR(from, to, fn) PP_IF(PP_EQ(from, to), ,fn(from)) PP_IF(PP_EQ(from, to), , PP_DEFER_4(__PP_FOR_I) () (PP_INC(from), to, fn))\n");
+    printf("#define __PP_FOR_I() __PP_FOR\n");
+}
+
+
---
+++ pp_test.c
@@ -0,0 +1,41 @@
+#include "pp.h"
+
+#include <assert.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    assert(0 == PP_BOOL(0)  && "Unexpected boolean value");
+    assert(1 == PP_BOOL(1)  && "Unexpected boolean value");
+    assert(1 == PP_BOOL(10) && "Unexpected boolean value");
+    
+    assert(5 == PP_ADD(3, 2) && "Unexpected arithmetic value");
+    assert(6 == PP_MUL(3, 2) && "Unexpected arithmetic value");
+    assert(1 == PP_SUB(3, 2) && "Unexpected arithmetic value");
+    assert(2 == PP_DIV(4, 2) && "Unexpected arithmetic value");
+
+    assert(10 == PP_MUL(PP_ADD(2, 3), 2) && "Unexpected arithmetic value");
+
+    assert(1 == PP_BOOL(PP_ADD(2, 3)) && "Unexpected boolean value");
+
+    assert(0 == PP_LEN()        && "Unexpected len");
+    assert(1 == PP_LEN(1)       && "Unexpected len");
+    assert(2 == PP_LEN(1, 1)    && "Unexpected len");
+    assert(3 == PP_LEN(1, 1, 1) && "Unexpected len");
+
+    assert('f' == PP_IF(0, 't', 'f') && "Unexpected if result");
+    assert('t' == PP_IF(1, 't', 'f') && "Unexpected if result");
+    assert('t' == PP_IF(2, 't', 'f') && "Unexpected if result");
+
+    
+    do {
+#define INC_X(it) ++x;
+        int x = 0;
+        PP_FOR(0, 10, INC_X);
+        assert(x == 10);
+#undef INC_X
+    } while(0);
+    
+    printf("passed\n");
+    return 0;
+}
